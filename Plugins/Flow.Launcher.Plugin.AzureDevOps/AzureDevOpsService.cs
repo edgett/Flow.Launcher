f8f466ea17353e4e1d546908ee60fa49b8db7609
Hilimire, Edgett <ehilimire@pertainium.com>
2023-02-21 23:41:52 -0500

    Querying devops.

--- Plugins/Flow.Launcher.Plugin.AzureDevOps/AzureDevOpsService.cs
+++ Plugins/Flow.Launcher.Plugin.AzureDevOps/AzureDevOpsService.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Documents;
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.TeamFoundation.Work.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualBasic;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
 
@@ -13,6 +18,8 @@ namespace Flow.Launcher.Plugin.AzureDevOps
 {
     public class AzureDevOpsService
     {
+        private ProjectHttpClient _projectClient;
+
         public AzureDevOpsService(AzureDevOpsSettings settings)
         {
             this.Settings = settings;
@@ -20,20 +27,23 @@ namespace Flow.Launcher.Plugin.AzureDevOps
             var devOpsUri = new Uri(settings.DevOpsUrl);
             VssConnection = new VssConnection(devOpsUri, creds);
 
+
         }
 
         public VssConnection VssConnection { get; }
         public AzureDevOpsSettings Settings { get; }
 
 
-        public async IAsyncEnumerable<TeamProjectReference> ListProjectsAsync()
+        public async IAsyncEnumerable<TeamProjectReference> ListProjectsAsync(CancellationToken cancellationToken = default)
         {
-            var projectClient = await VssConnection.GetClientAsync<ProjectHttpClient>();
+            var projectClient = await VssConnection.GetClientAsync<ProjectHttpClient>(cancellationToken);
             var projects = await projectClient.GetProjects();
+
+
 
             foreach (var p in projects)
             {
-                yield return p;   
+                yield return p;
             }
 
             while (!string.IsNullOrWhiteSpace(projects.ContinuationToken))
@@ -45,6 +55,79 @@ namespace Flow.Launcher.Plugin.AzureDevOps
                     yield return p;
                 }
             }
+        }
+
+        public async IAsyncEnumerable<WorkItem> SearchWorkItems(string search, CancellationToken cancellationToken = default)
+        {
+            var allProjects = ListProjectsAsync(cancellationToken);
+            var allProjectsEnum = allProjects.GetAsyncEnumerator();
+            while (await allProjectsEnum.MoveNextAsync())
+            {
+                var workItems = SearchWorkItems(search, allProjectsEnum.Current.Name, cancellationToken);
+                var workItemsEnum = workItems.GetAsyncEnumerator();
+                while (await workItemsEnum.MoveNextAsync())
+                {
+                    yield return workItemsEnum.Current;
+                }
+            }
+        }
+
+        public async IAsyncEnumerable<WorkItem> SearchWorkItems(string search, IEnumerable<string> projects, CancellationToken cancellationToken = default)
+        {
+            foreach (var project in projects)
+            {
+                var pjtWorkItems = SearchWorkItems(search, project, cancellationToken);
+                var pjtEnum = pjtWorkItems.GetAsyncEnumerator(cancellationToken);
+                while (await pjtEnum.MoveNextAsync())
+                {
+                    yield return pjtEnum.Current;
+                }
+            }
+        }
+
+        public async IAsyncEnumerable<WorkItem> SearchWorkItems(string search, string project, CancellationToken cancellationToken = default)
+        {
+            var workItemClient = await VssConnection.GetClientAsync<WorkItemTrackingHttpClient>();
+
+            var wiql = new Wiql();
+            wiql.Query = buildWiql(search, project);
+
+            var workItemsQueryResult = await workItemClient.QueryByWiqlAsync(wiql, top: 10, cancellationToken: cancellationToken);
+           
+            foreach (var wi in workItemsQueryResult.WorkItems)
+            {
+                var workItem = await workItemClient.GetWorkItemAsync(project, wi.Id, fields:new List<string>() { "System.Title", "System.TeamProject" }, cancellationToken: cancellationToken);
+                yield return workItem;
+            }
+
+
+        }
+
+        private string buildWiql(string search, string project)
+        {
+            search = escapeWiQl(search);
+
+            var wiqlString = $@"Select [Id] 
+                    From WorkItems 
+                    Where 
+                    [System.TeamProject] = '{project}'
+                    and
+                    (
+                        [Title] CONTAINS '{search}'
+                        or
+                        [Description] Contains Words '{string.Join("*", search.Split(" "))}'
+                    )
+                    Order By [Changed Date] Desc";
+
+
+            return wiqlString;
+        }
+
+        private string escapeWiQl(string input)
+        {
+            var output = input.Replace("'", "''");
+            output = output.Replace("\\", "\\\\");
+            return output;
         }
 
         public static async Task<(bool, string)> CheckConfig(AzureDevOpsSettings settings)
--- Plugins/Flow.Launcher.Plugin.AzureDevOps/Main.cs
+++ Plugins/Flow.Launcher.Plugin.AzureDevOps/Main.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.Services.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Flow.Launcher.Plugin.AzureDevOps
     public class Main : IAsyncPlugin, ISettingProvider
     {
         private PluginInitContext _context;
+        private AzureDevOpsService? _devOpsService;
         private AzureDevOpsSettings _azureDevOpsSettings;
 
         public Main()
@@ -38,32 +40,74 @@ namespace Flow.Launcher.Plugin.AzureDevOps
             return settingsUi;
         }
 
-        public Task InitAsync(PluginInitContext context)
+        public async Task InitAsync(PluginInitContext context)
         {
-            _azureDevOpsSettings = _context.API.LoadSettingJsonStorage<AzureDevOpsSettings>();
             _context = context;
-            return Task.FromResult(true);
+            _azureDevOpsSettings = _context.API.LoadSettingJsonStorage<AzureDevOpsSettings>();
+
+            var (configOk, message) = await AzureDevOpsService.CheckConfig(_azureDevOpsSettings);
+            if (configOk) { 
+                _devOpsService = new AzureDevOpsService(_azureDevOpsSettings);
+            }
         }
 
-        public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
+        public async Task<List<Result>> QueryAsync(Query query, CancellationToken cancellationToken)
         {
             var results = new List<Result>();
-
-            results.Add(new Result
+            var userInputSplit = query.Search.Split(" ");
+            var devOpsSearch = "";
+            //Clean out the 
+            if (_context.CurrentPluginMetadata.ActionKeywords.Contains(userInputSplit[0]))
             {
-                Title = "Test",
-                SubTitle = "Test",
-                //IcoPath = "Images\\icon.png",
-                Action = e =>
-                {
-                    _context.API.ChangeQuery("Test");
-                    return false;
-                }
-            });
+                devOpsSearch = (userInputSplit.Length > 1) ? string.Join(" ", userInputSplit.Skip(1).Where(inputToken => !string.IsNullOrWhiteSpace(inputToken))) : userInputSplit[0];
+            }
+            else 
+            {
+                devOpsSearch = string.Join(" ", userInputSplit.Where(inputToken => !string.IsNullOrWhiteSpace(inputToken)));
+            }
 
+            if (string.IsNullOrWhiteSpace(devOpsSearch))
+            {
+                return new List<Result>();
+            }
+
+            
+            if (_devOpsService != null)
+            {
+                var workItems = _devOpsService.SearchWorkItems(query.Search, cancellationToken);
+                var workItemsEnum = workItems.GetAsyncEnumerator(cancellationToken);
+
+                while (await workItemsEnum.MoveNextAsync())
+                {
+                    var workItem = workItemsEnum.Current;
+                    results.Add(new Result
+                    {
+                        Title = (string)workItem.Fields["System.Title"],
+                        SubTitle = (string)workItem.Fields["System.TeamProject"],
+                        Action = e =>
+                        {
+                            _context.API.OpenUrl(workItem.Url);
+                            return true;
+                        }
+                    });
+                }
+            }
+
+            //results.Add(new Result
+            //{
+            //    Title = "Test",
+            //    SubTitle = "Test",
+            //    //IcoPath = "Images\\icon.png",
+            //    Action = e =>
+            //    {
+            //        _context.API.ChangeQuery("Test");
+            //        return false;
+            //    }
+            //});
 
             return results;
-            
         }
+
+       
     }
 }
